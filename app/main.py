34117fac79b32d6d9700198135bc675abce594ad
HARISH KUMAR SAINI <63179326+Harishsaini1812@users.noreply.github.com>
2026-06-13 11:10:20 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,10 +4,6 @@ from app.rag_pipeline import generate_answer
 
 app = FastAPI()
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
 class Query(BaseModel):
     question: str
 
