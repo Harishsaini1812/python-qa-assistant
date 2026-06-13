7b9ea35afdf187f0417ce9ae4b26ff60b7bf25f7
HARISH KUMAR SAINI <63179326+Harishsaini1812@users.noreply.github.com>
2026-06-13 10:59:11 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.rag_pipeline import generate_answer
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 app = FastAPI()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 class Query(BaseModel):
     question: str
